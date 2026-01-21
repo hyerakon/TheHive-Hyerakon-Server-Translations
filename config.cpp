ba9bf96ed26084b7b5cd41ca39bcf475a6df1e0d
Luigi Francesco Ricatti <ricattilf@gmail.com>
2026-01-21 13:36:37 +0100

    first commit

---
+++ config.cpp
@@ -0,0 +1,49 @@
+class CfgPatches
+{
+    class TheHive_Server_Translations_Scripts
+    {
+        units[]={};
+        weapons[]={};
+        requiredVersion=0.1;
+        requiredAddons[]=
+        {
+            "DayZExpansion_Quest_Scripts"
+        };
+    };
+};
+
+class CfgMods
+{
+	class TheHive_Server_Translations
+	{
+		action = "";
+		hideName = 0;
+		hidePicture = 0;
+		name = "The Hive - Hyerakon - Server Translations";
+		credits = "DayZ Expansion and Hyerakon";
+		author = "Hyerakon - Luigi Francesco Ricatti";
+		authorID = "";
+		version = "0.1";
+		extra = 0;
+		type = "mod";
+		dependencies[] = {"Game","World","Mission"};
+		class defs
+		{
+			class gameScriptModule
+			{
+				value = "";
+				files[] = {"TheHive_Hyerakon_Server_Translations/Scripts/3_Game"};
+			};
+			class worldScriptModule
+			{
+				value = "";
+				files[] = {"TheHive_Hyerakon_Server_Translations/Scripts/4_World"};
+			};
+            class missionScriptModule
+			{
+				value = "";
+				files[] = {"TheHive_Hyerakon_Server_Translations/Scripts/5_Mission"};
+			};
+		};
+	};
+};
---
+++ mod.cpp
@@ -0,0 +1,6 @@
+name = "The Hive - Hyerakon - Translations"; // name
+tooltip = "The Hive - Hyerakon - Translations"; // tool tip on mouse hover
+overview = "This mod is a custom Expansion Bundle–based faction system, developed exclusively for a private DayZ server focused on roleplay, faction conflict, and emergent gameplay."; // overview
+action = "https://www.thehivemind.it/"; // link
+author = "Hyerakon"; // author
+version = "1.5"; // version
